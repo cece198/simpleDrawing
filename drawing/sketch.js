5965b5fcc0d0b336b38e079c6f128de1322137ee
Cecelia Mauro <cece198@gmail.com>
2016-08-04 09:25:42 -0500

    simple drawing code

---
+++ drawing/sketch.js
@@ -0,0 +1,21 @@
+var r = 0;
+var g = 80;
+var b = 100;
+
+function circle(x, y, size) {
+  ellipse(x, y, size, size);
+}
+
+function setup() {
+  createCanvas(600,400);
+}
+
+function draw() {
+  noStroke();
+  fill(r, g, b);
+  circle(mouseX, mouseY, 25);
+}
+
+function mousePressed() {
+  clear();
+}
