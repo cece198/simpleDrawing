c1709e166fa03975705d427b9dad59868517d4d0
Cecelia Mauro <cece198@gmail.com>
2016-08-08 09:12:50 -0500

    trying loops

--- drawing/sketch.js
+++ drawing/sketch.js
@@ -8,8 +8,24 @@ function circle(x, y, size) {
 //set up the background
 function setup() {
   createCanvas(600,400);
-  background(0, 0, 0);
+  background(255,255,255);
+  
 }
+
+//function bullseye(x, y, d, nRings, color1, color2, color3){
+  //  noStroke();
+  //  fill(color1);
+  //  circle(x,y,d);
+  //  fill(color2);
+  //  circle(x, y, (2/3)*d);
+  //  fill(color3);
+  //  circle(x,y,(1/3)*d);
+    
+  //  for(var i=0; i<nRings; i++){
+  //    circle(x,y,d);
+  //    d = d/2; 
+  //  }
+//}
 
 function draw() {
   noStroke();
@@ -22,9 +38,16 @@ function draw() {
     myRed = 200;
     myBlue = 255;
   }
-  if (mouseIsPressed == true) {
-    noStroke();
-    fill(0, 0, 0);
-    circle(mouseX, mouseY, 100);
-  }
+  //if (mouseIsPressed == true) {
+   // noStroke();
+   // fill(0, 0, 0);
+   // circle(mouseX, mouseY, 100);
+ // }
+  
+  // function mousePressed(){
+    //color1=fill(40,40,40);
+    //color2=fill(30,30,30);
+    //color3=fill(20,20,20);
+    //bullseye(200,200,30,3,color1,color2,color3);
+  //} 
 }
